585da83c48c4ea67cb3c5d6b69ebed5479380a47
natsuki.ishibashi <16hn113l@rikkyo.ac.jp>
2021-02-14 08:00:00 +0000

    route

--- routes/web.php
+++ routes/web.php
@@ -25,5 +25,5 @@ Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
 Route::group(['middleware' => ['auth']], function () {
     
-    Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
+    Route::resource('tasks', 'TasksController', ['only' => ['index', 'create', 'store', 'show', 'edit', 'update', 'destroy']]);
 });
